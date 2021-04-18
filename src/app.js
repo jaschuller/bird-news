671d789ac7c47746b35e7b50ee967050b5bc0010
Schuller <Justin.Schuller@pega.com>
2021-04-18 12:57:15 -0400

    Added logic to hit proxy server and connect to Twitter search api

--- src/app.js
+++ src/app.js
@@ -3,29 +3,25 @@ import axios from "axios";
 import Search from "./components/Search";
 import Results from "./components/Results";
 import HashtagFilter from "./components/HashtagFilter";
+import { forEach } from "lodash";
 
-const DEFAULT_QUERY = "redux";
-const DEFAULT_HPP = "20"; // number of results returned per page of pagination
-
-//const PATH_BASE = "https://hn.algolia.com/api/v1";
-// change path base to an incorrect url like below to test error handling
-// const PATH_BASE = 'https://hn.algolia.com/api/v1xxxx';
-// const PATH_SEARCH = "/search";
-// const PARAM_SEARCH = "query=";
-// const PARAM_PAGE = "page=";
-// const PARAM_HPP = "hitsPerPage=";
+// Use cors-anywhere project deployed to heroku as a proxy server, in order to get around cross origin errors
+const PROXY_SERVER = "https://schuller-proxy.herokuapp.com/";
 
 // https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/api-reference/get-search-tweets
-// axios.defaults.baseURL = "https://api.twitter.com/1.1/search/tweets.json?";
-axios.defaults.headers.common["Authorization"] =
+const token =
   "AAAAAAAAAAAAAAAAAAAAAI4OHgEAAAAAlbk0HSIAqcc3havrrU9j2NeAQ34%3DzJmzwHuQerd8JJ2TeuHfqwKgBt6bK4tk93w3ocBB2vPuKMF3cG";
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
-const PATH_BASE = "https://api.twitter.com/1.1";
+axios.defaults.headers.common = { Authorization: `bearer ${token}` };
+const TWITTER_API = "https://api.twitter.com/1.1";
+const PATH_BASE = `${PROXY_SERVER}${TWITTER_API}`;
 const PATH_SEARCH = "/search/tweets.json";
 const PARAM_RESULT_TYPE = "result_type=popular";
 const PARAM_SEARCH = "q=";
-const PARAM_COUNT = 5;
+
+// Get only the first 5 tweets per designs
+const PARAM_COUNT = "count=5";
+
+let tweetResults = "none";
 
 // App is a derived class since it extends component
 class App extends Component {
@@ -35,7 +31,22 @@ class App extends Component {
     // super.method calls the parent method. super(...) here calls the parent constructor
     super(props);
 
-    this.state = {};
+    this.state = {
+      results: null,
+      error: null,
+    };
+  }
+
+  // Loop through the tweet results and build up a list of Results components
+  setFoundTweets(result) {
+    result.statuses.forEach(async function (status) {
+      console.log(status.user.profile_image_url);
+      console.log(status.user.name);
+      console.log(status.text);
+      status.entities.hashtags.forEach(async function (hashtag) {
+        console.log(hashtag.text);
+      });
+    });
   }
 
   // Axios instead of Fetch: Substitute the native Fetch API with Axios
@@ -49,10 +60,19 @@ class App extends Component {
     // TODO replace with a real search term once it is coming in from the Search component
     const placeholderSearchTerm = "science";
 
-    axios(
-      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${placeholderSearchTerm}&${PARAM_RESULT_TYPE}`
-    )
-      .then((result) => console.log(result.data))
+    // httpbin.org gives you the headers in the response
+    // body `res.data`.
+    // See: https://httpbin.org/#/HTTP_Methods/get_get
+    axios
+      .get(
+        `${PATH_BASE}${PATH_SEARCH}?${PARAM_RESULT_TYPE}&${PARAM_SEARCH}${placeholderSearchTerm}&${PARAM_COUNT}`,
+        {
+          headers: {
+            "X-Requested-With": "XMLHttpRequest",
+          },
+        }
+      )
+      .then((result) => this.setFoundTweets(result.data))
       .catch((error) => this.setState({ error }));
   }
 
@@ -60,14 +80,16 @@ class App extends Component {
   // TODO fetch some twitter results to populate initial render when the element is first mounted
   componentDidMount() {
     console.log("mounting...");
-
-    // Test out the axios call
-    // this.fetchSearchPopularTweets();
+    this.fetchSearchPopularTweets();
+    console.log(tweetResults);
   }
 
   render() {
     // ES6 use destructuring to set values
     const { searchTerm, results, searchKey, error, isLoading } = this.state;
+
+    // debugger;
+    console.log(tweetResults);
 
     return (
       <div className="page">
